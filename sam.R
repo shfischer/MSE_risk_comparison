d47c26d6316356bb28d546be2e23ad2c28ed7c48
shfischer <32540427+shfischer@users.noreply.github.com>
2021-07-28 09:45:28 +0100

    add some SAM explorations

--- sam.R
+++ sam.R
@@ -14,6 +14,184 @@ idx <- readRDS("input/model_input_idx.RDS")
 stk_XSA <- readRDS("input/stock_d.rds")
 
 ### ------------------------------------------------------------------------ ###
+### discards trials ####
+### ------------------------------------------------------------------------ ###
+
+### half of discards
+stk_d50 <- stk
+discards.n(stk_d50) <- discards.n(stk_d50)/2
+discards(stk_d50) <- computeDiscards(stk_d50)
+catch(stk_d50) <- computeCatch(stk_d50, slot = "all")
+
+plot(FLStocks(half = stk_d50, full = stk))
+fit_d50 <- FLR_SAM(stk = stk_d50, idx = idx)
+retro_d50 <- retro(fit_d50, year = 10)
+plot(retro_d50)
+retro_d50 <- retro(fit_d50, year = 5)
+mohn(retro_d50)
+
+### no discards before 2012
+stk_d2012 <- stk
+discards.n(stk_d2012)[, ac(1980:2011)] <- 0
+discards.wt(stk_d2012)[, ac(1980:2011)] <- 0
+discards(stk_d2012) <- computeDiscards(stk_d2012)
+catch(stk_d2012) <- computeCatch(stk_d2012, slot = "all")
+plot(FLStocks("2012" = stk_d2012, full = stk))
+fit_d2012 <- FLR_SAM(stk = stk_d2012, idx = idx)
+retro_d2012 <- retro(fit_d2012, year = 10)
+plot(retro_d2012)
+retro_d2012 <- retro(fit_d2012, year = 5)
+mohn(retro_d2012)
+# R(age 2)         SSB   Fbar(3-6) 
+# -0.18063963  0.05647796 -0.09316801 
+
+### no discards - retro worse & large patterns when going back >5 years
+stk_d0 <- stk
+discards.n(stk_d0) <- 0
+discards.wt(stk_d0) <- 0
+discards(stk_d0) <- computeDiscards(stk_d0)
+catch(stk_d0) <- computeCatch(stk_d0, slot = "all")
+plot(FLStocks("0" = stk_d0, full = stk))
+fit_d0 <- FLR_SAM(stk = stk_d0, idx = idx)
+retro_d0 <- retro(fit_d0, year = 10)
+plot(retro_d0)
+retro_d0 <- retro(fit_d0, year = 5)
+mohn(retro_d0)
+# R(age 2)        SSB  Fbar(3-6) 
+# -0.0259818  0.1446794 -0.1680583
+
+### catch at age 2 = 0
+stk_d_a2_0 <- stk
+catch.n(stk_d_a2_0)[1, ] <- NA
+fit_d_a2_0 <- FLR_SAM(stk = stk_d_a2_0, idx = idx)
+retro_d_a2_0 <- retro(fit_d_a2_0, year = 10)
+plot(retro_d_a2_0)
+retro_d_a2_0 <- retro(fit_d_a2_0, year = 5)
+mohn(retro_d_a2_0)
+# R(age 2)         SSB   Fbar(3-6) 
+# -0.12957232  0.09250615 -0.12563298
+
+### discard survival rates
+library(doParallel)
+cl <- makeCluster(5)
+registerDoParallel(cl)
+
+surv <- seq(0, 1, 0.05)
+names(surv) <- surv
+stks <- lapply(surv, function(x) {
+  stk_i <- stk
+  discards.n(stk_i) <- discards.n(stk_i) * (1 - x)
+  discards(stk_i) <- computeDiscards(stk_i)
+  catch(stk_i) <- computeCatch(stk_i, slot = "all")
+  return(stk_i)
+})
+fits <- foreach(x = stks, 
+                .packages = c("stockassessment", "FLfse")) %dopar% {
+  FLR_SAM(x, idx)
+}
+ll <- sapply(fits, function(x) c(x$opt$objective))
+# aics <- sapply(fits, AIC) # same pattern
+plot(x = surv, y = ll, type = "l")
+which.min(ll)
+# min at surv=0.29 ~ 0.3
+rets <- foreach(x = fits,
+                .packages = c("stockassessment")) %dopar% {
+  retro(x, year = 5)
+}
+mohns <- lapply(rets, mohn)
+mohns
+
+### lower plusgroup
+### need to reduce PG to 7 to see improvement...
+stk_pg <- setPlusGroup(stk, 7)
+idx_pg <- idx
+idx_pg$`FSP-7e` <- idx_pg$`FSP-7e`[ac(2:6), ]
+idx_pg$Q1SWBeam <- idx_pg$Q1SWBeam[ac(2:6), ]
+fit_pg <- FLR_SAM(stk = stk_pg, idx = idx_pg)
+fit_pg
+plot(fit_pg)
+ret_pg <- retro(fit_pg, year = 5)
+plot(ret_pg)
+mohn(ret_pg)
+
+### change idx ages
+### removing young/old ages from surveys makes fit worse/increases retro pattern
+idx_ages <- idx
+idx_ages$`FSP-7e` <- idx_ages$`FSP-7e`[ac(3:8), ]
+idx_ages$Q1SWBeam <- idx_ages$Q1SWBeam[ac(3:7), ]
+fit_ages <- FLR_SAM(stk = stk, idx = idx_ages)
+fit_ages
+plot(fit_ages)
+ret_ages <- retro(fit_ages, year = 5)
+plot(ret_ages)
+mohn(ret_ages)
+
+### change SAM configuration
+### keyVarObs - coupling of observation variance parameters - bit better
+conf_i <- fit$conf
+conf_i$keyVarObs[2, 1:7] <- c(1, 1, rep(2, 3), 3, 3)
+conf_i$keyVarObs[3, 1:8] <- c(4, 4,  rep(5, 4), 6, 6)
+conf_i$keyVarObs
+fit_conf <- FLR_SAM(stk = stk, idx = idx, conf = conf_i, conf_full = TRUE)
+fit_conf
+plot(fit_conf)
+ret_conf <- retro(fit_conf, year = 5)
+plot(ret_conf)
+mohn(ret_conf)
+
+### keyLogFsta - coupling of F states - tiny improvement
+conf_i <- fit$conf
+conf_i$keyLogFsta[1, 1:9] <- c(0:5, 6, 6, 6)
+conf_i$keyLogFsta
+fit_conf <- FLR_SAM(stk = stk, idx = idx, conf = conf_i, conf_full = TRUE)
+fit_conf
+plot(fit_conf)
+ret_conf <- retro(fit_conf, year = 5)
+plot(ret_conf)
+mohn(ret_conf)
+
+### keyLogFpar - coupling of survey catchability - minor
+conf_i <- fit$conf
+conf_i$keyLogFpar[2, 1:7] <- c(0, 0, 1:3, 4, 4)
+conf_i$keyLogFpar[3, 1:8] <- c(5, 5, 6:9, 10, 10)
+conf_i$keyLogFpar
+fit_conf <- FLR_SAM(stk = stk, idx = idx, conf = conf_i, conf_full = TRUE)
+fit_conf
+plot(fit_conf)
+ret_conf <- retro(fit_conf, year = 5)
+plot(ret_conf)
+mohn(ret_conf)
+
+### keyVarF - coupling of F random walk - minor
+conf_i <- fit$conf
+conf_i$keyVarF[1, ] <- c(0, 0, 1, 1, 1, 1, 1, 2, 2)
+conf_i$keyVarF
+fit_conf <- FLR_SAM(stk = stk, idx = idx, conf = conf_i, conf_full = TRUE)
+fit_conf
+plot(fit_conf)
+ret_conf <- retro(fit_conf, year = 5)
+plot(ret_conf)
+mohn(ret_conf)
+
+### remove surveys
+### remove Q1SWBeam - minor impact
+### remove FSP - large retro
+lo <- leaveout(fit)
+plot(lo)
+fit_i <- FLR_SAM(stk = stk, idx = idx$`FSP-7e`)
+fit_i
+plot(fit_i)
+ret_i <- retro(fit_i, year = 5)
+plot(ret_i)
+mohn(ret_i)
+fit_i <- FLR_SAM(stk = stk, idx = idx$Q1SWBeam)
+fit_i
+plot(fit_i)
+ret_i <- retro(fit_i, year = 5)
+plot(ret_i)
+mohn(ret_i)
+
+### ------------------------------------------------------------------------ ###
 ### fit SAM with default configuration ####
 ### ------------------------------------------------------------------------ ###
 
