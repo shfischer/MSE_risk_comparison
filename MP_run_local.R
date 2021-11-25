647d9a0384a4b97f6bf1fc80695ca567a617cc76
shfischer <32540427+shfischer@users.noreply.github.com>
2021-11-25 20:09:11 +0000

    add some more local runs for cod and rfb rule

--- MP_run_local.R
+++ MP_run_local.R
@@ -1,6 +1,19 @@
 ### ------------------------------------------------------------------------ ###
 ### script for running MSE locally (not on HPC) ####
 ### ------------------------------------------------------------------------ ###
+suppressMessages(library(FLCore))
+suppressMessages(library(FLash))
+suppressMessages(library(FLBRP))
+suppressMessages(library(mse))
+suppressMessages(library(FLfse))
+suppressMessages(library(FLXSA))
+suppressMessages(library(GA))
+suppressMessages(library(doParallel))
+suppressMessages(library(doRNG))
+suppressMessages(library(tidyr))
+suppressMessages(library(dplyr))
+suppressMessages(library(stockassessment))
+suppressMessages(library(doParallel))
 
 req_pckgs <- c("FLCore", "FLash", "FLBRP", "mse", "FLfse", "FLXSA",
                "GA", "doParallel", "doRNG",
@@ -25,7 +38,7 @@ cl_length <- length(cl)
 }
 
 ### ------------------------------------------------------------------------ ###
-### rfb rule - optimised parameterisations ####
+### ple.27.7e: rfb rule - optimised parameterisations ####
 ### ------------------------------------------------------------------------ ###
 
 ### PA - multiplier = 0.95
@@ -59,7 +72,7 @@ source("MP_run.R")
 
 
 ### ------------------------------------------------------------------------ ###
-### rfb rule & alternative OMs ####
+### ple.27.7e: rfb rule & alternative OMs ####
 ### ------------------------------------------------------------------------ ###
 
 ### with multiplier = 1.16 (optimised)
@@ -207,7 +220,7 @@ for (OM in OMs) {
 
 
 ### ------------------------------------------------------------------------ ###
-### 2 over 3 rule ####
+### ple.27.7e: 2 over 3 rule ####
 ### ------------------------------------------------------------------------ ###
 ### baseline OM
 args_local <- c("n_blocks=10", "n_workers=0", "scenario=''", 
@@ -238,3 +251,199 @@ for (OM in OMs) {
    source("MP_run.R")
 }
 
+### ------------------------------------------------------------------------ ###
+### cod.27.47d20: rfb rule - optimised parameterisations ####
+### ------------------------------------------------------------------------ ###
+
+### PA - multiplier = 0.95
+args_local <- c("n_blocks=10", "n_workers=0", "scenario=''", "MP='rfb'",
+                "n_yrs=20", "check_file=FALSE",
+                "stock_id='cod.27.47d20'", "OM='baseline'", "save_MP=TRUE",
+                "popSize=1", "maxiter=1",
+                "multiplier=0.95", "add_suggestions=FALSE", "collate=FALSE")
+source("MP_run.R")
+### optimised - multiplier = 1.63
+args_local <- c("n_blocks=10", "n_workers=0", "scenario=''", "MP='rfb'",
+                "n_yrs=20", "check_file=FALSE",
+                "stock_id='cod.27.47d20'", "OM='baseline'", "save_MP=TRUE",
+                "popSize=1", "maxiter=1",
+                "multiplier=1.63", "add_suggestions=FALSE", "collate=FALSE")
+source("MP_run.R")
+### optimised - all parameters
+args_local <- c("n_blocks=10", "n_workers=0", "scenario=''", "MP='rfb'",
+                "n_yrs=20", "check_file=FALSE",
+                "stock_id='cod.27.47d20'", "OM='baseline'", "save_MP=TRUE",
+                "popSize=1", "maxiter=1",
+                "add_suggestions=FALSE", "collate=FALSE",
+                "lag_idx=0.7046149", "range_idx_1=2.635445", 
+                "range_idx_2=3.240444",
+                "range_catch=1", "exp_r=0.1971575", "exp_f=0.6958902", 
+                "exp_b=1.129193", "interval=4.086768", "multiplier=1.176044",
+                "upper_constraint=1.2", "lower_constraint=0.7")
+source("MP_run.R")
+
+rm(lag_idx, range_idx_1, range_idx_2, range_catch, exp_r, exp_f, exp_b, 
+   interval, multiplier, upper_constraint, lower_constraint)
+
+### ------------------------------------------------------------------------ ###
+### cod.27.47d20: rfb rule - alternative OMs ####
+### ------------------------------------------------------------------------ ###
+
+### PA - multiplier = 0.95
+### OM: rec_higher
+rm(lag_idx, range_idx_1, range_idx_2, range_catch, exp_r, exp_f, exp_b, 
+   interval, multiplier, upper_constraint, lower_constraint, rec_failure)
+args_local <- c("n_blocks=10", "n_workers=0", "scenario=''", "MP='rfb'",
+                "n_yrs=20", "check_file=FALSE",
+                "stock_id='cod.27.47d20'", "OM='rec_higher'", "save_MP=TRUE",
+                "popSize=1", "maxiter=1",
+                "multiplier=0.95", "add_suggestions=FALSE", "collate=FALSE")
+source("MP_run.R")
+### OM: M_dd
+rm(lag_idx, range_idx_1, range_idx_2, range_catch, exp_r, exp_f, exp_b, 
+   interval, multiplier, upper_constraint, lower_constraint, rec_failure)
+args_local <- c("n_blocks=10", "n_workers=0", "scenario=''", "MP='rfb'",
+                "n_yrs=20", "check_file=FALSE",
+                "stock_id='cod.27.47d20'", "OM='M_dd'", "save_MP=TRUE",
+                "popSize=1", "maxiter=1",
+                "multiplier=0.95", "add_suggestions=FALSE", "collate=FALSE")
+source("MP_run.R")
+### OM: M_no_migration
+rm(lag_idx, range_idx_1, range_idx_2, range_catch, exp_r, exp_f, exp_b, 
+   interval, multiplier, upper_constraint, lower_constraint, rec_failure)
+args_local <- c("n_blocks=10", "n_workers=0", "scenario=''", "MP='rfb'",
+                "n_yrs=20", "check_file=FALSE",
+                "stock_id='cod.27.47d20'", "OM='M_no_migration'", "save_MP=TRUE",
+                "popSize=1", "maxiter=1",
+                "multiplier=0.95", "add_suggestions=FALSE", "collate=FALSE")
+source("MP_run.R")
+### OM: rec_failure
+rm(lag_idx, range_idx_1, range_idx_2, range_catch, exp_r, exp_f, exp_b, 
+   interval, multiplier, upper_constraint, lower_constraint, rec_failure)
+args_local <- c("n_blocks=10", "n_workers=0", "scenario='rec_failure'", 
+                "MP='rfb'", "rec_failure=2021:2025",
+                "n_yrs=20", "check_file=FALSE",
+                "stock_id='cod.27.47d20'", "OM='baseline'", "save_MP=TRUE",
+                "popSize=1", "maxiter=1",
+                "multiplier=0.95", "add_suggestions=FALSE", "collate=FALSE")
+source("MP_run.R")
+
+### optimised multiplier = 1.63
+### OM: rec_higher
+rm(lag_idx, range_idx_1, range_idx_2, range_catch, exp_r, exp_f, exp_b, 
+   interval, multiplier, upper_constraint, lower_constraint, rec_failure)
+args_local <- c("n_blocks=10", "n_workers=0", "scenario=''", "MP='rfb'",
+                "n_yrs=20", "check_file=FALSE",
+                "stock_id='cod.27.47d20'", "OM='rec_higher'", "save_MP=TRUE",
+                "popSize=1", "maxiter=1",
+                "multiplier=1.63", "add_suggestions=FALSE", "collate=FALSE")
+source("MP_run.R")
+### OM: M_dd
+rm(lag_idx, range_idx_1, range_idx_2, range_catch, exp_r, exp_f, exp_b, 
+   interval, multiplier, upper_constraint, lower_constraint, rec_failure)
+args_local <- c("n_blocks=10", "n_workers=0", "scenario=''", "MP='rfb'",
+                "n_yrs=20", "check_file=FALSE",
+                "stock_id='cod.27.47d20'", "OM='M_dd'", "save_MP=TRUE",
+                "popSize=1", "maxiter=1",
+                "multiplier=1.63", "add_suggestions=FALSE", "collate=FALSE")
+source("MP_run.R")
+### OM: M_no_migration
+rm(lag_idx, range_idx_1, range_idx_2, range_catch, exp_r, exp_f, exp_b, 
+   interval, multiplier, upper_constraint, lower_constraint, rec_failure)
+args_local <- c("n_blocks=10", "n_workers=0", "scenario=''", "MP='rfb'",
+                "n_yrs=20", "check_file=FALSE",
+                "stock_id='cod.27.47d20'", "OM='M_no_migration'", "save_MP=TRUE",
+                "popSize=1", "maxiter=1",
+                "multiplier=1.63", "add_suggestions=FALSE", "collate=FALSE")
+source("MP_run.R")
+### OM: rec_failure
+rm(lag_idx, range_idx_1, range_idx_2, range_catch, exp_r, exp_f, exp_b, 
+   interval, multiplier, upper_constraint, lower_constraint, rec_failure)
+args_local <- c("n_blocks=10", "n_workers=0", "scenario='rec_failure'", 
+                "MP='rfb'", "rec_failure=2021:2025",
+                "n_yrs=20", "check_file=FALSE",
+                "stock_id='cod.27.47d20'", "OM='baseline'", "save_MP=TRUE",
+                "popSize=1", "maxiter=1",
+                "multiplier=1.63", "add_suggestions=FALSE", "collate=FALSE")
+source("MP_run.R")
+
+### all parameters optimised
+### OM: rec_higher
+rm(lag_idx, range_idx_1, range_idx_2, range_catch, exp_r, exp_f, exp_b, 
+   interval, multiplier, upper_constraint, lower_constraint, rec_failure)
+args_local <- c("n_blocks=10", "n_workers=0", "scenario=''", "MP='rfb'",
+                "n_yrs=20", "check_file=FALSE",
+                "stock_id='cod.27.47d20'", "OM='rec_higher'", "save_MP=TRUE",
+                "popSize=1", "maxiter=1",
+                "add_suggestions=FALSE", "collate=FALSE",
+                "lag_idx=0.7046149", "range_idx_1=2.635445", 
+                "range_idx_2=3.240444",
+                "range_catch=1", "exp_r=0.1971575", "exp_f=0.6958902", 
+                "exp_b=1.129193", "interval=4.086768", "multiplier=1.176044",
+                "upper_constraint=1.2", "lower_constraint=0.7")
+source("MP_run.R")
+### OM: M_dd
+rm(lag_idx, range_idx_1, range_idx_2, range_catch, exp_r, exp_f, exp_b, 
+   interval, multiplier, upper_constraint, lower_constraint, rec_failure)
+args_local <- c("n_blocks=10", "n_workers=0", "scenario=''", "MP='rfb'",
+                "n_yrs=20", "check_file=FALSE",
+                "stock_id='cod.27.47d20'", "OM='M_dd'", "save_MP=TRUE",
+                "popSize=1", "maxiter=1",
+                "add_suggestions=FALSE", "collate=FALSE",
+                "lag_idx=0.7046149", "range_idx_1=2.635445", 
+                "range_idx_2=3.240444",
+                "range_catch=1", "exp_r=0.1971575", "exp_f=0.6958902", 
+                "exp_b=1.129193", "interval=4.086768", "multiplier=1.176044",
+                "upper_constraint=1.2", "lower_constraint=0.7")
+source("MP_run.R")
+### OM: M_no_migration
+rm(lag_idx, range_idx_1, range_idx_2, range_catch, exp_r, exp_f, exp_b, 
+   interval, multiplier, upper_constraint, lower_constraint, rec_failure)
+args_local <- c("n_blocks=10", "n_workers=0", "scenario=''", "MP='rfb'",
+                "n_yrs=20", "check_file=FALSE",
+                "stock_id='cod.27.47d20'", "OM='M_no_migration'", "save_MP=TRUE",
+                "popSize=1", "maxiter=1",
+                "add_suggestions=FALSE", "collate=FALSE",
+                "lag_idx=0.7046149", "range_idx_1=2.635445", 
+                "range_idx_2=3.240444",
+                "range_catch=1", "exp_r=0.1971575", "exp_f=0.6958902", 
+                "exp_b=1.129193", "interval=4.086768", "multiplier=1.176044",
+                "upper_constraint=1.2", "lower_constraint=0.7")
+source("MP_run.R")
+### OM: rec_failure
+rm(lag_idx, range_idx_1, range_idx_2, range_catch, exp_r, exp_f, exp_b, 
+   interval, multiplier, upper_constraint, lower_constraint, rec_failure)
+args_local <- c("n_blocks=10", "n_workers=0", "scenario='rec_failure'", 
+                "MP='rfb'", "rec_failure=2021:2025",
+                "n_yrs=20", "check_file=FALSE",
+                "stock_id='cod.27.47d20'", "OM='baseline'", "save_MP=TRUE",
+                "popSize=1", "maxiter=1",
+                "add_suggestions=FALSE", "collate=FALSE",
+                "lag_idx=0.7046149", "range_idx_1=2.635445", 
+                "range_idx_2=3.240444",
+                "range_catch=1", "exp_r=0.1971575", "exp_f=0.6958902", 
+                "exp_b=1.129193", "interval=4.086768", "multiplier=1.176044",
+                "upper_constraint=1.2", "lower_constraint=0.7")
+source("MP_run.R")
+
+
+
+
+
+
+
+
+
+
+### ------------------------------------------------------------------------ ###
+### tmp - cod - SAM tests ####
+### ------------------------------------------------------------------------ ###
+
+if (FALSE) {
+  args_local <- c("n_iter=1000", "n_blocks=1000", "n_workers=10", "scenario=''", 
+                  "MP='ICES_SAM'",
+                  "n_yrs=20", "check_file=FALSE", "ga_search=FALSE",
+                  "stock_id='cod.27.47d20'", "OM='baseline'", "save_MP=TRUE",
+                  "stat_yrs='multiple'", "collate=FALSE")
+  source("MP_run.R")
+}
