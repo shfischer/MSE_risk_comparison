8835be171231647ead29a1c71fa6adb900aad884
shfischer <32540427+shfischer@users.noreply.github.com>
2022-01-21 11:50:32 +0000

    update ple OMs to FLasher

--- OM_ple.27.7e.R
+++ OM_ple.27.7e.R
@@ -51,8 +51,9 @@ create_OM(stk_data = stk_data, idx_data = idx_data, n = 1000, n_years = 100,
           SAM_conf = SAM_conf, SAM_newtonsteps = 0, SAM_rel.tol = 0.001,
           n_sample_yrs = 5, sr_model = "bevholtSV", sr_parallel = 10,
           sr_ar_check = TRUE, process_error = TRUE, catch_oem_error = TRUE,
-          idx_weights = c("catch.wt", "stock.wt"), idxB = "FSP-7e", idxL = TRUE,
-          ALK_yrs = 2016:2020, length_samples = 2000, PA_status = TRUE,
+          idx_weights = c("catch.wt", "stock.wt"), idxB = "FSP-7e", 
+          idxL = TRUE, ALKs = ALKs, ALK_yrs = 2016:2020, length_samples = 2000,
+          PA_status = TRUE,
           refpts = refpts, stock_id = "ple.27.7e", OM = "baseline", save = TRUE,
           return = FALSE, M_alternative = NULL)
 ### high M: +50%
@@ -61,8 +62,9 @@ create_OM(stk_data = stk_data, idx_data = idx_data, n = 1000, n_years = 100,
           SAM_conf = SAM_conf, SAM_newtonsteps = 0, SAM_rel.tol = 0.001,
           n_sample_yrs = 5, sr_model = "bevholt", sr_parallel = 10,
           sr_ar_check = TRUE, process_error = TRUE, catch_oem_error = TRUE,
-          idx_weights = c("catch.wt", "stock.wt"), idxB = "FSP-7e", idxL = TRUE,
-          ALK_yrs = 2016:2020, length_samples = 2000, PA_status = TRUE,
+          idx_weights = c("catch.wt", "stock.wt"), idxB = "FSP-7e", 
+          idxL = TRUE, ALKs = ALKs, ALK_yrs = 2016:2020, length_samples = 2000,
+          PA_status = TRUE,
           refpts = refpts, stock_id = "ple.27.7e", OM = "M_high", save = TRUE,
           return = FALSE, M_alternative = 0.18)
 ### low M: -50%
@@ -71,8 +73,9 @@ create_OM(stk_data = stk_data, idx_data = idx_data, n = 1000, n_years = 100,
           SAM_conf = SAM_conf, SAM_newtonsteps = 0, SAM_rel.tol = 0.001,
           n_sample_yrs = 5, sr_model = "bevholt", sr_parallel = 10,
           sr_ar_check = TRUE, process_error = TRUE, catch_oem_error = TRUE,
-          idx_weights = c("catch.wt", "stock.wt"), idxB = "FSP-7e", idxL = TRUE,
-          ALK_yrs = 2016:2020, length_samples = 2000, PA_status = TRUE,
+          idx_weights = c("catch.wt", "stock.wt"), idxB = "FSP-7e", 
+          idxL = TRUE, ALKs = ALKs, ALK_yrs = 2016:2020, length_samples = 2000,
+          PA_status = TRUE,
           refpts = refpts, stock_id = "ple.27.7e", OM = "M_low", save = TRUE,
           return = FALSE, M_alternative = 0.06)
 ### Gislason age-dependent M 
@@ -87,8 +90,9 @@ create_OM(stk_data = stk_data, idx_data = idx_data, n = 1000, n_years = 100,
           SAM_conf = SAM_conf, SAM_newtonsteps = 0, SAM_rel.tol = 0.001,
           n_sample_yrs = 5, sr_model = "bevholt", sr_parallel = 10,
           sr_ar_check = TRUE, process_error = TRUE, catch_oem_error = TRUE,
-          idx_weights = c("catch.wt", "stock.wt"), idxB = "FSP-7e", idxL = TRUE,
-          ALK_yrs = 2016:2020, length_samples = 2000, PA_status = TRUE,
+          idx_weights = c("catch.wt", "stock.wt"), idxB = "FSP-7e", 
+          idxL = TRUE, ALKs = ALKs, ALK_yrs = 2016:2020, length_samples = 2000,
+          PA_status = TRUE,
           refpts = refpts, stock_id = "ple.27.7e", OM = "M_Gislason", 
           save = TRUE, return = FALSE, M_alternative = M_age)
 ### default OM but no auto-correlated recruitment
@@ -97,14 +101,18 @@ create_OM(stk_data = stk_data, idx_data = idx_data, n = 1000, n_years = 100,
           SAM_conf = SAM_conf, SAM_newtonsteps = 0, SAM_rel.tol = 0.001,
           n_sample_yrs = 5, sr_model = "bevholt", sr_parallel = 10,
           sr_ar_check = FALSE, process_error = TRUE, catch_oem_error = TRUE,
-          idx_weights = c("catch.wt", "stock.wt"), idxB = "FSP-7e", idxL = TRUE,
-          ALK_yrs = 2016:2020, length_samples = 2000, PA_status = TRUE,
+          idx_weights = c("catch.wt", "stock.wt"), idxB = "FSP-7e", 
+          idxL = TRUE, ALKs = ALKs, ALK_yrs = 2016:2020, length_samples = 2000,
+          PA_status = TRUE,
           refpts = refpts, stock_id = "ple.27.7e", OM = "rec_no_AC", save = TRUE,
           return = FALSE, M_alternative = NULL)
 
 # plot(FLStocks(default = stk, M_low = stk_M_low, M_high = stk_M_high, M_Gislason = stk_M_Gislason), probs = c(0.05, 0.25, 0.5, 0.75, 0.95)) + xlim(c(NA, 2020))
 
 ### no discards - 100% discard survival
+### - assume 0 discards in OM, but MP assumes discards do happen
+### (discards appear in OM stk for easier conditioning of OM, but do not 
+###  affect stock numbers or F)
 # debugonce(create_OM)
 create_OM(stk_data = stk_data, idx_data = idx_data, n = 1000, n_years = 100,
           yr_data = 2020,
@@ -112,20 +120,23 @@ create_OM(stk_data = stk_data, idx_data = idx_data, n = 1000, n_years = 100,
           n_sample_yrs = 5, sr_model = "bevholt", sr_parallel = 10, 
           sr_start = 1990,
           sr_ar_check = TRUE, process_error = TRUE, catch_oem_error = TRUE,
-          idx_weights = c("catch.wt", "stock.wt"), idxB = "FSP-7e", idxL = TRUE,
-          ALK_yrs = 2016:2020, length_samples = 2000, PA_status = TRUE,
+          idx_weights = c("catch.wt", "stock.wt"), idxB = "FSP-7e", 
+          idxL = TRUE, ALKs = ALKs, ALK_yrs = 2016:2020, length_samples = 2000,
+          PA_status = TRUE,
           refpts = refpts, stock_id = "ple.27.7e", OM = "no_discards",
           save = TRUE, return = FALSE, M_alternative = NULL,
           disc_survival = 1, disc_survival_hidden = TRUE)
-### repeat but exclude dead discards - used for MSY calculation
+### repeat but exclude dead discards in OM
+### only used for MSY calculation,
 create_OM(stk_data = stk_data, idx_data = idx_data, n = 1000, n_years = 100,
           yr_data = 2020,
           SAM_conf = SAM_conf, SAM_newtonsteps = 0, SAM_rel.tol = 0.001,
           n_sample_yrs = 5, sr_model = "bevholt", sr_parallel = 10,
           sr_start = 1990,
           sr_ar_check = TRUE, process_error = TRUE, catch_oem_error = TRUE,
-          idx_weights = c("catch.wt", "stock.wt"), idxB = "FSP-7e", idxL = TRUE,
-          ALK_yrs = 2016:2020, length_samples = 2000, PA_status = TRUE,
+          idx_weights = c("catch.wt", "stock.wt"), idxB = "FSP-7e", 
+          idxL = TRUE, ALKs = ALKs, ALK_yrs = 2016:2020, length_samples = 2000,
+          PA_status = TRUE,
           refpts = refpts, stock_id = "ple.27.7e", 
           OM = "no_discards_not_hidden",
           save = TRUE, return = FALSE, M_alternative = NULL,
@@ -172,10 +183,14 @@ if (FALSE) {
 ### find ratio of R(SSB=Blim)/R0 -> definition of Blim
 stk_baseline <- readRDS("input/ple.27.7e/baseline/1000_100/stk.rds")
 Blim <- min(iterMedians(ssb(stk_baseline)), na.rm = TRUE)
+dimnames(stk_baseline)$year[which.min(iterMedians(ssb(stk_baseline)))]
+### Blim is SSB in 2007
 sr_baseline <- readRDS("input/ple.27.7e/baseline/1000_100/sr.rds")
 RR0 <- c(((iterMedians(params(sr_baseline)["a"])*Blim) /
             (iterMedians(params(sr_baseline))["b"] + Blim)) /
   iterMedians(params(sr_baseline))["a"])
+RR0
+### Blim corresponds to SSB at ~ 77% of R0
 
 refpts <- FLPar(refpts, iter = 1000, unit = "")
 update_refpts <- function(stock_id = "ple.27.7e", OM, refpts, RR0) {
@@ -208,6 +223,7 @@ update_refpts(OM = "M_Gislason", refpts = refpts, RR0 = RR0)
 ### no recruitment AC
 update_refpts(OM = "rec_no_ac", refpts = refpts, RR0 = RR0)
 ### 100% discards survival
+### - use no_discards_not_hidden OM where discards are not included in OM stk
 update_refpts(OM = "no_discards_not_hidden", refpts = refpts, RR0 = RR0)
 file.copy(from = "input/ple.27.7e/no_discards_not_hidden/1000_100/refpts_mse.rds",
           to = "input/ple.27.7e/no_discards/1000_100/refpts_mse.rds", 
